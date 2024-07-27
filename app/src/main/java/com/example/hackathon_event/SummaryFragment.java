c0359dc5598a6e99f3c038c4cbba07ce7a9e8c2a
XiuhuiL66 <xiuhuikilo@gmail.com>
2024-07-26 22:50:43 -0400

    update summaryFragment

---
+++ app/src/main/java/com/example/hackathon_event/SummaryFragment.java
@@ -0,0 +1,18 @@
+package com.example.hackathon_event;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class SummaryFragment extends Fragment {
+    private TextView summaryTextView;
+    private Button backButton;
+
+   
+}
