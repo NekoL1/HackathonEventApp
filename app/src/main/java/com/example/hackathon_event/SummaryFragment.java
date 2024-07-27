0ce52acc5a8bbc47b0df6e92a192e7735fa0088c
XiuhuiL66 <xiuhuikilo@gmail.com>
2024-07-26 22:51:05 -0400

    update summaryFragment

--- app/src/main/java/com/example/hackathon_event/SummaryFragment.java
+++ app/src/main/java/com/example/hackathon_event/SummaryFragment.java
@@ -14,5 +14,16 @@ public class SummaryFragment extends Fragment {
     private TextView summaryTextView;
     private Button backButton;
 
-   
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_summary, container, false);
+
+        summaryTextView = view.findViewById(R.id.summaryTextView);
+        backButton = view.findViewById(R.id.backButton);
+
+       
+
+        return view;
+    }
 }
